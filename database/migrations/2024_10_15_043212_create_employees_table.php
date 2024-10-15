25245b47237e64fdbd601d8cbfa68fbe6d75ae1a
dynaldynerd <dl_aldyn@yahoo.co.id>
2024-10-15 11:44:55 +0700

    add: constraint employee

--- database/migrations/2024_10_15_043212_create_employees_table.php
+++ database/migrations/2024_10_15_043212_create_employees_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('country_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('state_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('city_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('department_id')->constrained()->cascadeOnDelete();
             $table->string('first_name');
             $table->string('last_name');
             $table->string('middle_name');
